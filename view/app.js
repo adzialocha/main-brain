3adaf0548f5a1fc095a527c387ad3bf7e34bdf83
Andreas Dzialocha <andreas@marmorkuchen.net>
2016-11-30 21:36:08 +0100

    Nicer view

--- view/app.js
+++ view/app.js
@@ -2,13 +2,47 @@
   'use strict';
 
   var SCORE_PATH = '/score';
-  var ANIMATION_DURATION = 1000;
+  var ANIMATION_DURATION = 1500;
   var WS_ADDRESS = '192.168.178.115';
   var WS_PORT = 9090;
 
+  var DEFAULT_NODE_COLOR = '#ff1122';
+  var SELECTED_NODE_COLOR = '#000';
+
+  var GRAPH_STYLES = [
+    {
+      selector: 'node',
+      style: {
+        'width': 150,
+        'height': 150,
+        'background-color': DEFAULT_NODE_COLOR,
+        'label': 'data(id)',
+        'text-valign': 'center',
+        'color': '#fff',
+        'text-background-color': '#000',
+        'text-background-opacity': 1,
+        'selection-color': '#333',
+      },
+    }, {
+      selector: 'edge',
+      style: {
+        'width': 7,
+        'line-color': '#000',
+        'target-arrow-color': '#000',
+        'target-arrow-shape': 'triangle',
+        'curve-style': 'unbundled-bezier',
+        'source-label': 'data(name)',
+        'color': '#fff',
+        'text-background-color': '#000',
+        'text-background-opacity': 1,
+        'source-text-offset': 30
+      },
+    },
+  ];
+
   // private
 
-  var graph;
+  var graph, currentNodeId;
 
   function loadScore() {
     return $.ajax({
@@ -21,16 +55,20 @@
     graph.animate({
       center: {
         eles: graph.nodes("[id='" + id + "']"),
-      }
+      },
     }, {
       duration: ANIMATION_DURATION,
     });
 
-    graph.$("[id='" + id + "']").select();
+    graph.$('[id="' + currentNodeId + '"]').style('backgroundColor', DEFAULT_NODE_COLOR);
+    graph.$('[id="' + id + '"]').style('backgroundColor', SELECTED_NODE_COLOR);
+
+    currentNodeId = id;
   }
 
   function changeDensity(density) {
-    $('body').css('background-color', 'rgba(255, 50, 50, ' + density + ')');
+    var rgba = [250, 50, 50, density];
+    $('body').css('background-color', 'rgba(' + rgba.join('/') + ')');
     $('#density').html(Math.round(density * 100) / 100);
   }
 
@@ -56,36 +94,7 @@
 
       graph = cytoscape({
         container: $('#graph'),
-        style: [
-          {
-            selector: 'node',
-            style: {
-              'width': 150,
-              'height': 150,
-              'background-color': '#ff1122',
-              'label': 'data(id)',
-              'text-valign': 'center',
-              'color': '#fff',
-              'text-background-color': '#000',
-              'text-background-opacity': 1,
-              'selection-color': '#333',
-            },
-          }, {
-            selector: 'edge',
-            style: {
-              'width': 7,
-              'line-color': '#000',
-              'target-arrow-color': '#000',
-              'target-arrow-shape': 'triangle',
-              'curve-style': 'unbundled-bezier',
-              'source-label': 'data(name)',
-              'color': '#fff',
-              'text-background-color': '#000',
-              'text-background-opacity': 1,
-              'source-text-offset': 30
-            },
-          },
-        ],
+        style: GRAPH_STYLES,
         zoomingEnabled: true,
         userZoomingEnabled: false,
         panningEnabled: true,
@@ -116,13 +125,12 @@
                 name: edge.treshold.join('-'),
                 source: id,
                 target: edge.node,
-              }
+              },
             });
           });
         });
 
         graph.add(edges);
-
         graph.layout({ name: 'circle' });
 
         connectOSC();
